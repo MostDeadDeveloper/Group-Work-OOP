4de7b3a7862bd747d6e17e34720f2d81ddf1da1b
RandomUser <>
2020-12-15 00:01:58 +0800

    Added repetitive input requests

--- group_work_1/Group3_BSCS2_2Act2.java
+++ group_work_1/Group3_BSCS2_2Act2.java
@@ -4,43 +4,49 @@ import java.util.*;
 /**
  * Group 3: BSCS 2-2 Object Oriented Programming
  * Members:
- * 			Iñigo, Cherrieanne Lontoc L.
- * 			Yusi, Ericka Mae S.
- * 			Vinas, Carl John R.
+ *          Iñigo, Cherrieanne Lontoc L.
+ *          Yusi, Ericka Mae S.
+ *          Vinas, Carl John R.
 */
 
 public class Group3_BSCS2_2Act2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String repeatCondition = "N";
+        do {
+            System.out.println("Enter first word: ");
+            String firstWord = scan.nextLine();
+            firstWord = firstWord.replaceAll("\\s","");
+            System.out.println("Enter second word: ");
+            String secondWord = scan.nextLine();
+            secondWord = secondWord.replaceAll("\\s","");
 
-        System.out.println("Enter first word: ");
-        String firstWord = scan.nextLine();
-        firstWord = firstWord.replaceAll("\\s","");
-        System.out.println("Enter second word: ");
-        String secondWord = scan.nextLine();
-        secondWord = secondWord.replaceAll("\\s","");
+            boolean status = true;
 
-        boolean status = true;
+            if (firstWord.length() != secondWord.length()) {
+                status = false;
+            }
+            else {
+                char[] firstWordArray = firstWord.toLowerCase().toCharArray();
+                char [] secondWordArray = secondWord.toLowerCase().toCharArray();
 
-        if (firstWord.length() != secondWord.length()) {
-            status = false;
-        }
-        else {
-            char[] firstWordArray = firstWord.toLowerCase().toCharArray();
-            char [] secondWordArray = secondWord.toLowerCase().toCharArray();
+                Arrays.sort(firstWordArray);
+                Arrays.sort(secondWordArray);
 
-            Arrays.sort(firstWordArray);
-            Arrays.sort(secondWordArray);
+                status = Arrays.equals(firstWordArray, secondWordArray);
+            }
 
-            status = Arrays.equals(firstWordArray, secondWordArray);
-        }
+            if (status) {
+                System.out.println("Output: Anagram!");
+            }
+            else {
+                System.out.println("Output: Not Anagram!");
+            }
 
-        if (status) {
-            System.out.println("Output: Anagram!");
-        }
-        else {
-            System.out.println("Output: Not Anagram!");
-        }
+            System.out.print("Would You Like To Input Again? [y/n]: ");
+            repeatCondition = scan.nextLine();
+        }while(repeatCondition.equalsIgnoreCase("Y"));
+
     }
 }
 
